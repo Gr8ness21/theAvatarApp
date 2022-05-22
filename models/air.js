1c982034a2161e91d6e5d676bda192e8a939ae9c
Gr8ness <pmleechapman@gmail.com>
2022-05-22 01:45:35 -0400

    created index and show routes to start avatar app

---
+++ models/air.js
@@ -0,0 +1,22 @@
+// set up our mock database (this is totally temporary)
+const airChars = [
+    {
+        name: 'Aang',
+        town: 'Southern Air Temple',
+        mainChar: true
+    },
+    {
+        name: 'Gyatsu',
+        town: 'Southen Air Temple',
+        mainChar: false
+    },
+    {
+        name: 'Tenzin',
+        town: 'Republic City',
+        mainChar: true
+    },
+];
+
+module.exports = airChars;
+
+
---
+++ server.js
@@ -0,0 +1,80 @@
+// require dependencies
+const express = require("express");
+const methodOverride = require("method-override");
+
+const airChars = require("./models/air");
+
+// initialize express application
+const app = express();
+
+// configure application settings
+const port = 3000;
+
+// mount middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+
+// ALL ROUTES
+// Index
+app.get("/airChar", (req, res) => {
+	res.render("indexAir.ejs", { airChars: airChars });
+});
+
+// // New
+// app.get("/airChar/new", (req, res) => {
+// 	res.render("newAir.ejs");
+// });
+
+// // D
+// app.delete("/airChar/:indexOfAirCharArray", (req, res) => {
+// 	air.splice(req.params.indexOfAirCharArray, 1); //remove the item from the array
+// 	res.redirect("/airChar"); //redirect back to index route
+// });
+
+// // U
+// app.put("/airChar/:indexOfAirCharArray", (req, res) => {
+	
+// 	if (req.body.mainChar === "on") {
+// 		req.body.mainChar = true;
+// 	} else {
+// 		req.body.mainChar = false;
+// 	}
+// 	fruits[req.params.indexOfAirCharArray] = req.body;
+// 	res.redirect("/airChar");
+// });
+
+// // Create
+// app.post("/airChar", (req, res) => {
+// 	if (req.body.mainChar === "on") {
+// 		req.body.mainchar = true;
+// 	} else {
+// 		req.body.mainChar = false;
+// 	}
+// 	air.push(req.body);
+// 	console.log(airChars);
+// 	res.redirect("/airChar");
+// });
+
+// // E
+// app.get("/airChar/:indexOfAirCharArray/edit", (req, res) => {
+// 	res.render(
+// 		"editAir.ejs",
+// 		{
+// 			air: airChars[req.params.indexOfAirCharArray],
+// 			index: req.params.indexOfAirCharArray,
+// 		}
+// 	);
+// });
+
+// Show
+app.get("/airChar/:indexOfAirCharArray", (req, res) => {
+
+	res.render("showAir.ejs", {
+		airChars: airChars[req.params.indexOfAirCharArray],
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port`, port);
+});
